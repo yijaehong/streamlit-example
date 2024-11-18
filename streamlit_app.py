3c0d7ffc4c93f1a87c8980b816c3e0bc5a5d7a7d
yijaehong <132038641+yijaehong@users.noreply.github.com>
2024-11-18 22:17:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,17 +103,9 @@ with st.container():
             
             st.subheader("Pie Chart")
             
-            if platform.system() == 'Darwin': #맥
-                    plt.rc('font', family='AppleGothic') 
-            elif platform.system() == 'Windows': #윈도우
-                    plt.rc('font', family='Malgun Gothic') 
-            elif platform.system() == 'Linux': #리눅스 (구글 콜랩)
-                    #!wget "https://www.wfonts.com/download/data/2016/06/13/malgun-gothic/malgun.ttf"
-                    #!mv malgun.ttf /usr/share/fonts/truetype/
-                    #import matplotlib.font_manager as fm 
-                    #fm._rebuild() 
-                    plt.rc('font', family='Malgun Gothic') 
-            plt.rcParams['axes.unicode_minus'] = False #한글 폰트 사용시 마이너스 폰트 깨짐 해결
+            import matplotlib.font_manager as fm
+            fontprop = fm.FontProperties(fname='malgun.ttf')
+            #matplotlib 패키지 한글 폰트 설정 끝
 
          
             # plt.figure(figsize = (3,2))
